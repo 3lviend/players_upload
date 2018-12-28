9ffb2a586688d3822fe2fd84d7e4b310771a063f
Backend9OD <henryss_10@yahoo.com>
2018-12-28 09:25:25 +0700

    clean routing, delete unused file

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,19 +1,9 @@
 class ImagesController < ApplicationController
-  before_action :set_image, only: [:destroy]
 
-  # GET /images
-  # GET /images.json
-  def index
-    @images = Image.all
-  end
-
-  # GET /images/new
   def new
     @image = Image.new
   end
 
-  # POST /images
-  # POST /images.json
   def create
     success, @failed = [], []
     respond_to do |format|
@@ -34,16 +24,6 @@ class ImagesController < ApplicationController
     end
   end
 
-  # DELETE /images/1
-  # DELETE /images/1.json
-  def destroy
-    @image.destroy
-    respond_to do |format|
-      format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   def play_random
     size       = rand(1..10)
     @images    = Image.all.map{ |p| [p.file.url, p.id] }.sample(size)
@@ -57,11 +37,6 @@ class ImagesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_image
-      @image = Image.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
       params.require(:image).permit({files: []}, :file)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :images, only: [:index, :new, :create, :destroy]
+  resources :images, only: [:new, :create]
   resources :plays, only: [:create]
   get 'play_random' => 'images#play_random', as: :play_random
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'images#index'
+  root 'images#new'
 end
